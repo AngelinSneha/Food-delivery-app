cd901d1ba1ed4c5b1432c22b4e96be9379bf13a1
angelin <angelinsneha91@gmail.com>
2020-12-29 22:02:12 +0530

    adding Restaurants Page

---
+++ frontend/src/Appbar.js
@@ -0,0 +1,154 @@
+import React from 'react';
+import {  makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import PersonIcon from '@material-ui/icons/Person';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
+import MenuIcon from '@material-ui/icons/Menu';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  root: {
+    flexGrow: 1,
+  },
+  title: {
+    flexGrow: 1,
+    fontFamily:'helvetica',
+    fontSize:'1.4rem'
+  },
+  appbar: {
+    backgroundColor:'#c70039'
+  },
+  button: {
+    color:"white",
+    fontSize:'0.7rem',
+    marginRight:'.5rem'
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('sm')]: {
+      display: 'none',
+    },
+  },
+}));
+
+export default function Appbar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+
+  // const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+      <Button
+        startIcon={<ShoppingCartIcon />}
+      >
+        Cart
+      </Button>
+      </MenuItem>
+      <MenuItem>
+        <Button
+        startIcon={<PersonIcon />}
+      >
+        help
+      </Button>
+      </MenuItem>
+      <MenuItem>
+      <Button
+        startIcon={<ExitToAppIcon />}
+      >
+        Logout
+      </Button>
+        </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <div className={classes.grow}>
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+           🍜 Food Delivery
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.sectionDesktop}>
+          <Button
+        className={classes.button}
+        startIcon={<ShoppingCartIcon />}
+      >
+        Cart
+      </Button>
+            <Button
+        className={classes.button}
+        startIcon={<PersonIcon />}
+      >
+        help
+      </Button>
+            <Button
+        className={classes.button}
+        startIcon={<ExitToAppIcon />}
+      >
+        Logout
+      </Button>
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+    </div>
+  );
+}
--- frontend/src/RestaurantsHomePage.js
+++ frontend/src/RestaurantsHomePage.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import Appbar from "./Appbar";
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import "./RestaurantsHomePage.css";
+import Footer from "./Footer"
 function RestaurantsHomePage() {
     return (
         <div>
-            hi
+            {/* AppBar */}
+            <Appbar />
+            {/* content and restaurants */}
+            <div className="content">
+            <span className="address">Home  -&gt;  Bangalore  -&gt; <span className="addressSpan">Food Delivery</span></span>
+            <h1 className="OrderfromRes">Order From Restaurants Around You</h1>
+            <hr></hr>
+            </div>
+            
+
+            {/* FOOTER */}
+            <Footer />
         </div>
     )
 }
